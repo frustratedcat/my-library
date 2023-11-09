8a6378d05b704bc05a436ff092d73e5843a89bfb
frustratedcat <jaredbrown@skiff.com>
2023-11-09 10:54:04 -0500

    Add exit button for modal and add javascript funcitonality for opening and closing

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 "use strict";
 
+const addItemBtn = document.querySelector(".add-item-btn");
+const addItemModal = document.querySelector(".add-item-modal");
+const exitModalBtn = document.querySelector(".exit-modal");
+
 const myLibrary = [];
 
 function Book() {}
 
 function addBookToLibrary() {}
+
+const addItem = function () {
+  addItemBtn.addEventListener("click", () => {
+    console.log("Open Add Item Modal");
+    addItemModal.classList.remove("hide-modal");
+  });
+};
+
+const exitModal = function () {
+  exitModalBtn.addEventListener("click", () => {
+    console.log("Close Modal");
+    addItemModal.classList.add("hide-modal");
+  });
+};
+
+const run = function () {
+  addItem();
+  exitModal();
+};
+
+run();
